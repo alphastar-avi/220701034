39febb9cff61d12432da3d9a94766375aacdfb3c
Avinash <avinash592004@gmail.com>
2025-07-05 11:46:09 +0530

    backend

--- Backend/utils/urlShortner.go
+++ Backend/utils/urlShortner.go
@@ -1,33 +1,43 @@
 package urlshortner
 
-import(
-	"fmt"
-	"net/http"
-	"encoding/json"
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "time"
 )
 
 func SetupRoute() {
-	http.HandleFunc("/shorturls", UrlHandler)
+    http.HandleFunc("/shorturls", UrlHandler)
 }
 
 func UrlHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
-		return
-	}
+    if r.Method != http.MethodPost {
+        http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-	type Message struct {
-		Url       string `json:"url"`
-		Validity  int    `json:"validity"`
-		Shortcode string `json:"shortcode"`
-	}
+    type Request struct {
+        Url       string `json:"url"`
+        Validity  int    `json:"validity"`
+        Shortcode string `json:"shortcode"`
+    }
 
-	var msg Message
-	err := json.NewDecoder(r.Body).Decode(&msg)
-	if err != nil {
-		http.Error(w, "invalid JSON", http.StatusBadRequest)
-		return
-	}
-	fmt.Print(msg.Url, msg.Validity, msg.Shortcode)
-	fmt.Fprintln(w, "test done")
+    var req Request
+    err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, "invalid json", http.StatusBadRequest)
+        return
+    }
+
+    expiry := time.Now().Add(time.Duration(req.Validity) * time.Minute).UTC().Format(time.RFC3339)
+    shortLink := fmt.Sprintf("https://%s/%s", r.Host, req.Shortcode)
+
+    resp := map[string]string{
+        "shortLink": shortLink,
+        "expiry":    expiry,
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(resp)
 }
